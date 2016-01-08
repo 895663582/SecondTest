1c4433dbd1b9689cffca5a0180ffdcb323de01b1
asd <895663582@qq.com>
2016-01-08 16:36:05 +0800

    123

---
+++ 15/15/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  15
+//
+//  Created by asd on 16/1/8.
+//  Copyright © 2016年 asd. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
